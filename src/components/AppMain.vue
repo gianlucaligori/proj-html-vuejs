9e3dd26b4efe4c5b0b78c7fb11f3e2e9e243c150
gianlucaligori <gianluca.ligori.95@gmail.com>
2023-05-30 11:16:28 +0200

    add third section

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -2,6 +2,7 @@
 </script>
 
 <template>
+    <!-- INIZIO JUMBO -->
     <div class="jumbo d-flex flex-column justify-content-center align-items-center">
 
         <h2>Inspiring Tomorrow Leaders</h2>
@@ -15,7 +16,7 @@
 
 
 
-
+    <!-- PRIMA SEZIONE -->
     <div class="container first_cont">
         <section class="first_sec text-center">
             <img class="logo_lil p-2" src="../assets/images/page-logo-1.png" alt="">
@@ -39,6 +40,7 @@
                 </div>
 
                 <div class="col-6">
+                    <!-- VIDEO QUI -->
                     <iframe width="450" height="300" src="https://www.youtube.com/embed/tgbNymZ7vqY">
                     </iframe>
                 </div>
@@ -47,23 +49,138 @@
     </div>
 
 
-    <div class="container">
-        <div class="row m-5">
 
-            <div class="col-6">
-                <h3>WORLD CLASS FACILITIES</h3>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem, doloribus. Omnis, quis incidunt quo tempora
-                    quia tenetur voluptatibus deserunt commodi magni quasi reprehenderit at ipsa sit autem non fugit
-                    placeat.</p>
+    <!-- SECONDA SEZIONE -->
+    <div class="container second_cont">
+        <section>
+            <div class="row m-5">
 
-                <ul>
-                    <li>
-                        <span>International focus:</span> <span>Our students</span>
-                    </li>
-                </ul>
+                <div class="col-6">
+                    <h3>WORLD CLASS FACILITIES</h3>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem, doloribus. Omnis, quis incidunt quo
+                        tempora
+                        quia tenetur voluptatibus deserunt commodi magni quasi reprehenderit at ipsa sit autem non fugit
+                        placeat.</p>
+
+                    <ul>
+                        <li>
+                            <span>International focus:</span> <span>Our students</span>
+                        </li>
+                        <li>
+                            <span>International focus:</span> <span>Our students</span>
+                        </li>
+                        <li>
+                            <span>International focus:</span> <span>Our students</span>
+                        </li>
+                    </ul>
+                </div>
+                <!-- ACCORDION TENDINA 5 ELEMENTI -->
+                <div class="col-6">
+                    <div class="accordion accordion-flush" id="accordionFlushExample">
+                        <div class="accordion-item">
+                            <h2 class="accordion-header">
+                                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+                                    data-bs-target="#flush-collapseOne" aria-expanded="false"
+                                    aria-controls="flush-collapseOne">
+                                    Accordion Item #1
+                                </button>
+                            </h2>
+                            <div id="flush-collapseOne" class="accordion-collapse collapse"
+                                data-bs-parent="#accordionFlushExample">
+                                <div class="accordion-body">Placeholder content for this accordion, which is intended to
+                                    demonstrate the <code>.accordion-flush</code> class. This is the first item's accordion
+                                    body.</div>
+                            </div>
+                        </div>
+                        <div class="accordion-item">
+                            <h2 class="accordion-header">
+                                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+                                    data-bs-target="#flush-collapseTwo" aria-expanded="false"
+                                    aria-controls="flush-collapseTwo">
+                                    Accordion Item #2
+                                </button>
+                            </h2>
+                            <div id="flush-collapseTwo" class="accordion-collapse collapse"
+                                data-bs-parent="#accordionFlushExample">
+                                <div class="accordion-body">Placeholder content for this accordion, which is intended to
+                                    demonstrate the <code>.accordion-flush</code> class. This is the second item's accordion
+                                    body. Let's imagine this being filled with some actual content.</div>
+                            </div>
+                        </div>
+                        <div class="accordion-item">
+                            <h2 class="accordion-header">
+                                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+                                    data-bs-target="#flush-collapseThree" aria-expanded="false"
+                                    aria-controls="flush-collapseThree">
+                                    Accordion Item #3
+                                </button>
+                            </h2>
+                            <div id="flush-collapseThree" class="accordion-collapse collapse"
+                                data-bs-parent="#accordionFlushExample">
+                                <div class="accordion-body">Placeholder content for this accordion, which is intended to
+                                    demonstrate the <code>.accordion-flush</code> class. This is the third item's accordion
+                                    body. Nothing more exciting happening here in terms of content, but just filling up the
+                                    space to make it look, at least at first glance, a bit more representative of how this
+                                    would look in a real-world application.</div>
+                            </div>
+                            <div class="accordion-item">
+                                <h2 class="accordion-header">
+                                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+                                        data-bs-target="#flush-collapseOne" aria-expanded="false"
+                                        aria-controls="flush-collapseOne">
+                                        Accordion Item #1
+                                    </button>
+                                </h2>
+                                <div id="flush-collapseOne" class="accordion-collapse collapse"
+                                    data-bs-parent="#accordionFlushExample">
+                                    <div class="accordion-body">Placeholder content for this accordion, which is intended to
+                                        demonstrate the <code>.accordion-flush</code> class. This is the first item's
+                                        accordion
+                                        body.</div>
+                                </div>
+                            </div>
+                            <div class="accordion-item">
+                                <h2 class="accordion-header">
+                                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+                                        data-bs-target="#flush-collapseOne" aria-expanded="false"
+                                        aria-controls="flush-collapseOne">
+                                        Accordion Item #1
+                                    </button>
+                                </h2>
+                                <div id="flush-collapseOne" class="accordion-collapse collapse"
+                                    data-bs-parent="#accordionFlushExample">
+                                    <div class="accordion-body">Placeholder content for this accordion, which is intended to
+                                        demonstrate the <code>.accordion-flush</code> class. This is the first item's
+                                        accordion
+                                        body.</div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
+        </section>
+    </div>
 
-            <div class="col-6">
+
+    <!-- THREE CARDS -->
+    <div class="container_fluid third_sec three_cards">
+        <div class="row">
+            <div class="col-4 p-0 card_one">
+                <h3>COURSES</h3>
+                <p>View More</p>
+                <!-- <img src="../assets/images/home_courses_section_bg.jpg" alt=""> -->
+            </div>
+            <div class="col-4 p-0 card_two">
+                <h3>EVENTS</h3>
+                <p>View More</p>
+                <!-- <img src="../assets/images//home_events_section_bg.jpg" alt=""> -->
+
+            </div>
+            <div class="col-4 p-0 card_three">
+                <h3>LATEST NEWS</h3>
+                <p>View More</p>
+                <!-- <img src="../assets/images/home_latest_news_section_bg.jpg" alt=""> -->
 
             </div>
         </div>
@@ -109,7 +226,54 @@
     }
 }
 
-section {
-    margin-top: 8rem;
+// DA SISTEMARE QUI I MARGINI
+.container {
+    margin-top: 10rem;
+    margin-bottom: 5rem;
+}
+
+ul {
+    list-style: none;
+}
+
+.third_sec {
+    .row {
+        height: 700px;
+        max-width: 100%;
+    }
+
+    .card_one {
+        background-image: url(../assets/images/home_courses_section_bg.jpg);
+        background-size: cover;
+        background-repeat: no-repeat;
+    }
+
+    .card_two {
+        background-image: url(../assets/images/home_events_section_bg.jpg);
+        background-size: cover;
+        background-repeat: no-repeat;
+    }
+
+    .card_three {
+        background-image: url(../assets/images/home_latest_news_section_bg.jpg);
+        background-size: cover;
+        background-repeat: no-repeat;
+    }
+
+    h3,
+    p {
+        color: $dirtwhite;
+    }
+
+    .card_one,
+    .card_two,
+    .card_three {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        gap: 2rem;
+    }
+
 }
 </style>
